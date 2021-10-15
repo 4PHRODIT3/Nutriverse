68ed3bfd924250185ceae6faea628f88d03a5922
4PHRODIT3 <b8_htetmyatlinn@uit.edu.mm>
2021-10-15 19:16:12 +0630

    Update Navigation Bar and added Animations

--- assets/JS/customized-control.js
+++ assets/JS/customized-control.js
@@ -1,16 +1,19 @@
 // Nav-btn toggle icons
-let nav_btn = document.querySelector(".navbar-toggler");
-let nav_icon = document.querySelector("#nav-icon");
+let nav_close = document.querySelector("#nav-close");
+let mobile_nav = document.querySelector(".nav-open");
+let nav_open = document.querySelector("#nav-open");
 let nav_bar = document.querySelector(".navbar");
 let client_height = window.screen.height;
+let client_width = window.screen.width;
 
-nav_icon.addEventListener("click", e => {
-    e.preventDefault();
-})
-nav_btn.addEventListener("click", e => {
-    nav_icon.classList.toggle("fa-times");
-    nav_icon.classList.toggle("fa-bars");
-})
+$("#nav-open").click(function() {
+    $(".navbar").animate({ left: "0" });
+    $(this).hide();
+});
+$("#nav-close").click(function() {
+    $(".navbar").animate({ left: "-100%" });
+    $("#nav-open").show();
+});
 
 $('.package-collection').slick({
     dots: true,
@@ -18,48 +21,88 @@ $('.package-collection').slick({
     speed: 300,
     slidesToShow: 3,
     slidesToScroll: 3,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
-          infinite: true,
-          dots: true
+    responsive: [{
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true
+            }
+        },
+        {
+            breakpoint: 800,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
         }
-      },
-      {
-        breakpoint: 700,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2
-        }
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1
-        }
-      }
-      
+
     ]
-  });
-                  
+});
+
 $('.countings').counterUp({
     delay: 10,
 });
 
 window.onscroll = _ => {
-  let current_height = document.documentElement.scrollTop;
-  if(current_height > client_height - 100){
-    nav_bar.classList.add("navbar-on-scroll");
-  }
-  else{
-    nav_bar.classList.remove("navbar-on-scroll");
-  }
+    let current_height = document.documentElement.scrollTop;
+
+    if (current_height > client_height - 100) {
+        if (client_width > 870) {
+            nav_bar.classList.add("navbar-on-scroll");
+        } else {
+            mobile_nav.classList.add("navbar-on-scroll");
+        }
+    } else {
+        if (client_width > 870) {
+            nav_bar.classList.remove("navbar-on-scroll");
+        } else {
+            mobile_nav.classList.remove("navbar-on-scroll");
+        }
+    }
 };
 
 window.onload = _ => {
-  document.querySelector('.loader').remove();
+    document.querySelector('.loader').remove();
 }
+
+wow = new WOW({
+    boxClass: 'wow', // default
+    animateClass: 'animate__animated', // default
+    offset: 100, // default
+    mobile: true, // default
+    live: true // default
+})
+wow.init();
+
+const setActive = id => {
+
+    $(".nav-link").removeClass("active");
+    console.log($(`nav-link[href='#${id}']`));
+    $(`.nav-link[href='#${id}']`).addClass("active");
+
+}
+
+const setUpNavActions = _ => {
+    let sections = $("section[id]");
+    sections.waypoint(function(direction) {
+        let current_id = $(this.element).attr("id");
+        if (direction == "down") {
+            setActive(current_id);
+        } else {
+            setActive(current_id);
+        }
+    }, {
+        offset: "10%"
+    })
+}
+
+setUpNavActions();
